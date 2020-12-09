e0ad8e251afccb23be3508ce00db9b8f11a1b418
kochunghyo <kc19851226@gmail.com>
2020-12-09 22:30:56 +0900

    S3修正

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,21 +1,15 @@
-require 'carrierwave/storage/abstract'
-require 'carrierwave/storage/file'
-require 'carrierwave/storage/fog'
+CarrierWave.configure do |config|
+  config.storage = :fog
+  config.fog_provider = 'fog/aws'
+  config.fog_credentials = {
 
-if Rails.env.production?
-  CarrierWave.configure do |config|
-    config.storage = :fog
-    config.fog_provider = 'fog/aws'
-    config.fog_credentials = {
+    provider: 'AWS',
+    aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
+    aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
+    region: 'ap-northeast-1'
 
-      provider: 'AWS',
-      aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
-      aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
-      region: 'ap-northeast-1'
+  }
 
-    }
-
-    config.fog_directory  = 'asakatsu-output2020'
-    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/asakatsu-output2020'
-  end
+  config.fog_directory  = 'asakatsu-output2020'
+  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/asakatsu-output2020'
 end
