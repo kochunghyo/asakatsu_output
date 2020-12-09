b02e364a8fe77c3f38813adb7f0c7d8841e6fde1
kochunghyo <kc19851226@gmail.com>
2020-12-09 23:43:50 +0900

    バグ修正

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,7 @@ module AsakatsuOutput
     config.i18n.default_locale = :ja
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
+    config.assets.initialize_on_precompile = false
     # config.time
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
